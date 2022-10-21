e8fe91488aa3fc2cf6e48b81e315fccccc9cf104
dariacz12 <94304829+dariacz12@users.noreply.github.com>
2022-10-22 00:07:56 +0200

    Added Slider
    Co-authored-by: Daria Czupreta <dariaczupreta@iMac-Daria.local>

---
+++ src/consts/data.js
@@ -0,0 +1,24 @@
+import image1 from "../images/pexels-pavel-danilyuk-5789013.jpg";
+import image2 from "../images/n2.jpg";
+import image3 from "../images/n3.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: `${image1}`,
+    title: "SUMMER SALE",
+    description: "GET FLAT 30% OFF FOR NESW ARRIVALS",
+  },
+  {
+    id: 2,
+    img: `${image2}`,
+    title: "WINTER SALE",
+    description: "GET FLAT 40% OFF FOR NESW ARRIVALS",
+  },
+  {
+    id: 3,
+    img: `${image3}`,
+    title: "SPRING SALE",
+    description: "GET FLAT 50% OFF FOR NESW ARRIVALS",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Slider from "../components/Slider";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <Announcement/>
+      <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
