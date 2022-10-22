73837322f713235799d77df72cee72527be59283
Daria Czupreta <daria.czupreta@gmail.com>
2022-10-22 23:07:15 +0200

    Added Products

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,17 +5,24 @@ import CategoryItem from "./CategoryItem";
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+const Items = styled.div`
+  max-width: 1920px;
+  display: flex;
   padding: 50px 40px;
   justify-content: space-around;
   flex-wrap: wrap;
 `;
-
 const Categories = () => {
   return (
     <Container>
-      {categories.map((item) => (
-        <CategoryItem key={item.id} item={item} />
-      ))}
+      <Items>
+        {categories.map((item) => (
+          <CategoryItem key={item.id} item={item} />
+        ))}
+      </Items>
     </Container>
   );
 };
---
+++ src/components/Product.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 30px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40vh;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: all 0.5s ease;
+  &:hover {
+    opacity: 0.5;
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+`;
+
+const Product = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+    </Container>
+  );
+};
+
+export default Product;
