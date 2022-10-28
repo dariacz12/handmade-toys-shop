7140e29a6e88f3f7e32bed37519e9300931737f1
dariacz12 <94304829+dariacz12@users.noreply.github.com>
2022-10-28 19:53:55 +0200

    Added Contact Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Logotipe from "../images/logo.png";
 import footer from "../images/footer.jpg";
 import { devices } from "../consts/deviceSizes";
-import { Instagram, MailOutline, Phone, Room } from "@material-ui/icons";
+import ContactData from "./ContactData";
 
 const Container = styled.div`
   box-sizing: border-box;
@@ -61,27 +61,6 @@ const Right = styled.div`
   flex: 4;
   padding-top: 20px;
 `;
-const Title2 = styled.h1`
-  margin: 20px 0px;
-  padding-top: 10px;
-  font-size: 18px;
-  font-weight: 500;
-  letter-spacing: 2px;
-  color: whitesmoke;
-  text-shadow: 2px 1px 4px rgba(66, 68, 90, 1);
-  font-size: 14px;
-  @media ${devices.tablet} {
-    font-size: 18px;
-  }
-`;
-const ContactItem = styled.div`
-  margin-bottom: 12px;
-  display: flex;
-  align-items: center;
-  color: whitesmoke;
-  text-shadow: 2px 1px 4px rgba(66, 68, 90, 1);
-  cursor: pointer;
-`;
 
 const Footer = () => {
   return (
@@ -100,23 +79,7 @@ const Footer = () => {
         </List>
       </Left>
       <Right>
-        <Title2>KONTAKT</Title2>
-        <ContactItem>
-          <Room style={{ marginRight: "10px" }} />
-          Bielsko-Biała, Polska
-        </ContactItem>
-        <ContactItem>
-          <Phone style={{ marginRight: "10px" }} />
-          +48 883 172 180
-        </ContactItem>
-        <ContactItem>
-          <MailOutline style={{ marginRight: "10px" }} />
-          irenas@handmade.com
-        </ContactItem>
-        <ContactItem>
-          <Instagram style={{ marginRight: "10px" }} />
-          Sosialmedia
-        </ContactItem>
+        <ContactData />
       </Right>
     </Container>
   );
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,182 @@
+import React from "react";
+import ContactData from "../components/ContactData";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { devices } from "../consts/deviceSizes";
+import { useForm } from "react-hook-form";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: 0px 20px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media ${devices.laptop} {
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50px;
+  }
+`;
+const Title1 = styled.h1`
+  width: 100%;
+  color: #1a1a1a;
+  text-align: center;
+  margin-left: 20px;
+  @media ${devices.laptop} {
+    margin-bottom: 40px;
+  }
+`;
+const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  @media ${devices.laptop} {
+    flex-direction: row;
+  }
+`;
+const FormContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-bottom: ridge;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  border: none;
+  @media ${devices.laptop} {
+    margin: 10px;
+    padding: 20px;
+  }
+`;
+const UserData = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const Input1 = styled.input`
+  width: 100%;
+  padding: 10px 20px;
+  margin: 10px;
+  font-size: 16px;
+  border-color: gray;
+  border-width: 1px;
+`;
+const Input2 = styled.input`
+  width: 100%;
+  padding: 10px 20px;
+  margin: 10px;
+  font-size: 16px;
+  border-color: gray;
+  border-width: 1px;
+`;
+const TextField = styled.textarea`
+  margin: 10px;
+  padding: 10px;
+  font-size: 16px;
+  height: 250px;
+  border-color: gray;
+  border-width: 1px;
+`;
+const Input3 = styled.input`
+  padding: 10px;
+  margin: 10px;
+  font-size: 16px;
+  background-color: transparent;
+  color: gray;
+  border-color: gray;
+  border-width: 1px;
+  cursor: pointer;
+  background-color: teal;
+  color: white;
+  border: none;
+`;
+const Title2 = styled.h2`
+  padding-top: 50px;
+  margin-left: 10px;
+  color: teal;
+  @media ${devices.laptop} {
+    margin-left: 40px;
+  }
+`;
+const InfoContainer = styled.div`
+  color: #1a1a1a;
+  background-color: rgb(0, 128, 128, 0.1);
+  padding: 10px;
+  margin: 20px 10px 30px 10px;
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+  padding-left: 40px;
+  @media ${devices.laptop} {
+    justify-content: center;
+    margin: 10px 10px 0px 10px;
+  }
+`;
+const ContactPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title1>Masz pytania? Chętnie na nie odpowiem!</Title1>
+        <ContentContainer>
+          <FormContainer>
+            <Title2>NAPISZ WIADOMOŚĆ</Title2>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <UserData>
+                <Input1 {...register("name")} placeholder="Imię" />
+                <Input2
+                  {...register("email", { required: true, maxLength: 20 })}
+                  placeholder="Email"
+                />
+                {errors.email?.type === "required" && (
+                  <p role="alert">Email is required</p>
+                )}
+              </UserData>
+              <TextField
+                {...register("message")}
+                placeholder="Treść zapytania"
+              ></TextField>
+              <Input3 type="submit" />
+            </Form>
+          </FormContainer>
+          <InfoContainer>
+            <ContactData
+              styleText={{
+                fontSize: "20px",
+                color: "#1a1a1a",
+                textShadow: "none",
+              }}
+              styleTitle={{
+                textShadow: "none",
+                color: "teal",
+                fontSize: "24px",
+                fontWeight: "bold",
+              }}
+              styleIcon={{ marginRight: "20px" }}
+            />
+          </InfoContainer>
+        </ContentContainer>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default ContactPage;
